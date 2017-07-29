acbcbdad28768d4fb04c2bf0f6ef8cc885cb48c8
Tamás Bíró <tamas.biro@emarsys.com>
2017-07-29 18:53:11 +0200

    basic modal for newsletter

---
+++ app.js
@@ -0,0 +1,62 @@
+window.addEventListener('load', function load(event){
+  window.removeEventListener('load', load, false);
+  Modal.init();
+},false);
+
+var Modal = (function() {
+  var init = function() {
+    var modalhandlerElements = document.querySelectorAll('.modalhandler');
+    [].forEach.call(modalhandlerElements, function(modalHandlerElement) {
+      modalHandlerElement.addEventListener('click', _open);
+    });
+  };
+
+  var _open = function(event) {
+    event.preventDefault();
+
+    var id = _getModalIDFromElement(event.target);
+    if (!id) {
+      console.log('data-modal is missing');
+      return;
+    }
+
+    var modal = _searchModalByID(id);
+    if (!modal) {
+      console.log('modal #' + id + ' not found');
+      return;
+    }
+
+    _openModal(modal);
+    _bindModalEvents(modal);
+  }
+
+  var _close = function(event) {
+    var modalElement = event.target.parentNode;
+    _closeModal(modalElement);
+  }
+
+  var _openModal = function(element) {
+    element.classList.add('modal--opened')
+  }
+
+  var _closeModal = function(element) {
+    element.classList.remove('modal--opened')
+  }
+
+  var _bindModalEvents = function(element) {
+    var overlayElement = element.querySelector('.modal__overlay');
+    overlayElement.addEventListener('click', _close);
+  }
+
+  var _getModalIDFromElement = function(element) {
+    return element.getAttribute('data-modal');
+  }
+
+  var _searchModalByID = function(id) {
+    return document.querySelector('#' + id);
+  }
+
+  return {
+    init: init
+  };
+})();
